f1e2658577560d8e251e796c7cf5418c079000b4
Mauro Doneda <mauro.doneda@gmail.com>
2020-08-11 14:06:10 +0100

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SuperCarStore.MvcApplication" Language="C#" %>
---
+++ Models/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperCarStore.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public DateTime Year { get; set; }
+        public double HP { get; set; }
+        public string EngineSpec { get; set; }
+        public string FuelType { get; set; }
+        public double TopSpeed { get; set; }
+        public double ZeroTo60 { get; set; }
+        public double SalePrice { get; set; }
+        public double RentalPrice { get; set; }
+        public string ImgUrl { get; set; }
+        public Store Store { get; set; }
+        public int StoreId { get; set; }
+    }
+}
